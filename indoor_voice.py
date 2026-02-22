d9e65fad768fb2264558570efe957be3bb759912
saksheex <saksheesinghofc@gmail.com>
2026-02-23 00:16:29 +0530

    WRITING IN ALL CAPS IS LIKE YELLING

---
+++ indoor_voice.py
@@ -0,0 +1,11 @@
+def make_lower(a):
+    if any(char.isupper() for char in a):
+        print("Capital letter found, converting to lowercase\n")
+        return a.lower()
+    else:
+        print("You are good to go, no capitals found!\n")
+        return a
+
+text = input("Enter your inputs:\n ")
+result = make_lower(text)
+print("Final Version after review:", result)  
