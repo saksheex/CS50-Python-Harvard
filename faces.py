25d2cd0d6de61c0cf1b264b9e898e2de9e421963
saksheex <saksheesinghofc@gmail.com>
2026-02-23 00:27:02 +0530

    Magic

---
+++ faces.py
@@ -0,0 +1,6 @@
+def convert(text):
+     return text.replace(":)","🙂").replace(":(","🙁")
+
+text=input("Enter Your Message: ")
+result= convert(text)
+print("Magic Applied to your Message: ", result)
