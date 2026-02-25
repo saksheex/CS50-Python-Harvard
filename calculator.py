3fbc1afd883d60f75e158b6aa746ae489af42c39
saksheex <saksheesinghofc@gmail.com>
2026-02-25 16:06:09 +0530

    user made calculator

--- calculator.py
+++ calculator.py
@@ -1,15 +1,15 @@
-math_expression= input("Expression: ")
-var = math_expression.split()
+# math_expression= input("Expression: ")
+# var = math_expression.split()
 
-x = int(var[0])
-y = var[1]
-z = int(var[2])
+# x = int(var[0])
+# y = var[1]
+# z = int(var[2])
 
-if y == "+":
-     print(f"{x+z:.1f}")
-elif y == "-":
-     print(f"{x-z:.1f}")
-elif y== "*":
-     print(f"{x*z:.1f}")
-elif y == "/":
-     print(f"{x/z:.1f}")    
+# if y == "+":
+#      print(f"{x+z:.1f}")
+# elif y == "-":
+#      print(f"{x-z:.1f}")
+# elif y== "*":
+#      print(f"{x*z:.1f}")
+# elif y == "/":
+#      print(f"{x/z:.1f}")    
