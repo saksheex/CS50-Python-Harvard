6809576c9cc25ed205fe7636e8604368c891c9b6
saksheex <saksheesinghofc@gmail.com>
2026-03-06 19:13:04 +0530

    your message here

---
+++ font.py
@@ -0,0 +1,25 @@
+import pyfiglet
+import sys
+import random
+
+
+fonts = pyfiglet.FigletFont.getFonts()
+
+if len(sys.argv) == 1:
+    
+    font = random.choice(fonts)
+
+elif len(sys.argv) == 3:
+   
+    if sys.argv[1] not in ["-f", "--font"]:
+        sys.exit("Invalid flag. Use -f or --font")
+    if sys.argv[2] not in fonts:
+        sys.exit("Invalid font name.")
+    font = sys.argv[2]
+
+else:
+    sys.exit("Usage: python figlet.py [-f font]")
+
+text = input("Input: ")
+
+print(pyfiglet.figlet_format(text, font=font))
---
+++ game.py
@@ -0,0 +1,26 @@
+import random
+
+def get_int():
+    while True:
+        try:
+            user = int(input("Level: "))
+            if user > 0:
+             return user
+        except ValueError:
+            pass
+
+user_in = get_int()  
+r = random.randint(a=1,b=user_in)
+
+while True:
+    try:
+        guess = int(input("Guess: "))
+        if guess < r:
+            print("Too Small!")
+        elif guess > r:
+            print("Too Large!")
+        elif guess == r:
+            print("Just right!")
+            break
+    except ValueError:
+        pass
